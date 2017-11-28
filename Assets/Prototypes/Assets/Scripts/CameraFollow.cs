2de613b7fe42d63e7b9439386b77c2f5a2cc2ac7
keeganland <keeganland@gmail.com>
2017-11-27 20:21:29 -0800

    Modified CameraFollow with Alex's suggestion. Added comments to this script to reflect the discussions of the night

--- Assets/Prototypes/Assets/Scripts/CameraFollow.cs
+++ Assets/Prototypes/Assets/Scripts/CameraFollow.cs
@@ -20,10 +20,26 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     { 
+        /**
+         *  Keegan's NTS: 
+         *  Alex's intention with this if-block is "if the target is the player, etc" so that the behaviour in the block only takes place during normal gameplay circumstances.
+         *  It will not behave as such during e.g. a cutscene.
+         *  As it is, it is completely useless and will be modified during a future pass.
+         */
+
         if (target)
         {
+            //TODO: Cleanup
+            //Earlier incorrect line:
+            //transform.position = Vector3.Lerp(transform.position, targets[m_number].position, m_speed*Time.deltaTime) + new Vector3(0, 10f, 0);
+            //Slight modification
             //transform.position = Vector3.Lerp(transform.position, targets[m_number].position, m_speed * Time.deltaTime);
-            transform.position = Vector3.Lerp(transform.position, targets[m_number].position, m_speed*Time.deltaTime) + new Vector3(0, 10f, 0);
+            //Alex's proposed fix:
+
+            m_new_Pos = new Vector3(targets[m_number].position.x, this.transform.position.y, targets[m_number].position.z);
+
+            transform.position = Vector3.Lerp(transform.position, m_new_Pos, m_speed * Time.deltaTime);
+            
             ChangeCharacters();
         }
 

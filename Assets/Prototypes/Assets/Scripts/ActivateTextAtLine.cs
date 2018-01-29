bd89aadf3633abd121e60a1a91a87185743b11d4
Keegan-NewDesktop <keeganland@gmail.com>
2018-01-28 20:24:36 -0800

    OnTriggerExit buggy, commented out. Introduced textStarted variable for easy behind the scenes management
    Former-commit-id: 395a99892831af8a11800b5cb551b97cb8480b2d
    Former-commit-id: 497c9bd6c091236be4c719a9eaef35b8ae542c7d

--- Assets/Prototypes/Assets/Scripts/ActivateTextAtLine.cs
+++ Assets/Prototypes/Assets/Scripts/ActivateTextAtLine.cs
@@ -13,20 +13,22 @@ public class ActivateTextAtLine : MonoBehaviour {
 
     public bool requireButtonPress;
     public bool waitForPress;
-
-
     public bool destroyWhenActivated;
 
+    private bool textStarted; // If the text box has been activated, player should scroll through it before they get to end.
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         theTextManager = FindObjectOfType<TextBoxManager>();
+        textStarted = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (waitForPress && Input.GetKeyDown(KeyCode.Return))
+		if (waitForPress && Input.GetKeyDown(KeyCode.Return) && !textStarted)
         {
+            textStarted = true;
+
             theTextManager.ReloadScript(theText);
             theTextManager.currentLine = startLine;
             theTextManager.endAtLine = endLine;
@@ -38,12 +40,16 @@ public class ActivateTextAtLine : MonoBehaviour {
             }
         }
 
+        if(theTextManager.currentLine > theTextManager.endAtLine)
+        {
+            textStarted = false;
+        }
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if(other.name == "Boy")
-        //if (other.CompareTag("Player"))
+        //if(other.name == "Boy")
+        if (other.CompareTag("Player"))
         {
             if(requireButtonPress)
             {
@@ -62,12 +68,16 @@ public class ActivateTextAtLine : MonoBehaviour {
             }
         }
     }
-
+    /*
     void OnTriggerExit(Collider other)
     {
+        Debug.Log(other.name + " Trigger Exit");
+
         if(other.name == "Boy")
         {
             waitForPress = false;
+            Debug.Log("waitForPress = " + waitForPress);
+            //textStarted = false; //not sure where would be a better place for this, probably not here though.
         }
-    }
+    }*/
 }

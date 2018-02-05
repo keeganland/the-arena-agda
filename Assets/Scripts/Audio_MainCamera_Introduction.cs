0e56f8c427d4943dc92a0c753cc77b2bee00917c
Salade_de_Poney <alexandre.g007@gmail.com>
2018-02-04 18:29:06 -0800

    Alex : Sound Effect Work
    Former-commit-id: cc6e3204321cdee515b406140eec7a6cbdeec10e
    Former-commit-id: 586f6f25b02fffa9f421356e8782df6f1eafda42

---
+++ Assets/Scripts/Audio_MainCamera_Introduction.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio_MainCamera_Introduction : MonoBehaviour {
+
+    public AudioClip otherClip;
+
+
+    IEnumerator Start()
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+
+        yield return new WaitForSeconds((otherClip.length));
+        audio.Play();
+    }
+
+}

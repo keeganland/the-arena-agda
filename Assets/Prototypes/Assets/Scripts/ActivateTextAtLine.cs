7c337c2e3c30dac90a5335bd26c763d27330363b
Keegan-Thinkpad\Keegan <keeganland@gmail.com>
2018-03-08 20:14:41 -0800

    Talk bubbles finally working!!!
    Former-commit-id: b7679de8103e9159927cd3dc5c0a1f8797bdb8b2
    Former-commit-id: d849ce3f7da565285313da0c39f948e8b91f5c6f

--- Assets/Prototypes/Assets/Scripts/ActivateTextAtLine.cs
+++ Assets/Prototypes/Assets/Scripts/ActivateTextAtLine.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ActivateTextAtLine : MonoBehaviour {
+public class ActivateTextAtLine : MonoBehaviour
+{
 
-	/**
+    /**
 	 * Oh boy this one is getting messy.  
 	 * TODO:
 	 * does it even make sense to have this be ActivateTextAtLine going forward, or should it be simplified into ActivateText or something like that?
@@ -12,45 +13,50 @@ public class ActivateTextAtLine : MonoBehaviour {
 	 */
 
 
-	public string NPCName;
+    public string NPCName;
     public TextAsset theText;
-	public bool useSpeechBubble;
+    public bool useSpeechBubble;
 
     public int startLine;
     public int endLine;
 
     public TextBoxManager theTextManager;
-	public GameObject talkBubble;
+    public GameObject talkBubble;
 
     public bool requireButtonPress;
     public bool destroyWhenActivated;
+    public bool destroyWhenFinished;
     public bool tagTriggersText = true; //I'm thinking of trying to get it to trigger with the collider's name instead of tag. Is this feasible?
 
     public string activatedByTag;
     public string activatedByName;
 
-	private bool waitForPress = false;
+    private bool waitForPress = false;
     private bool textWasManuallyActivated; // If the text box has been activated, player should scroll through it before they get to end.
-	//man did i manage to confuse myself with the above variable's name!
+                                           //man did i manage to confuse myself with the above variable's name!
+    private bool destroyNextTimeTextboxCloses = false;
     private Player_Movement playerMover;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         theTextManager = FindObjectOfType<TextBoxManager>();
 
-		textWasManuallyActivated = false;
+        textWasManuallyActivated = false;
 
-		if (talkBubble != null) {
-			talkBubble.SetActive (false);
-		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-		if (waitForPress && Input.GetKeyDown(KeyCode.Return) && !textWasManuallyActivated)
+        if (talkBubble != null)
         {
-			textWasManuallyActivated = true;
+            talkBubble.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (waitForPress && Input.GetKeyDown(KeyCode.Return) && !textWasManuallyActivated)
+        {
+            textWasManuallyActivated = true;
 
             this.Activate();
 
@@ -59,19 +65,27 @@ public class ActivateTextAtLine : MonoBehaviour {
                 Destroy(gameObject);
             }
         }
-			
-		//Refactor to take into account the queue structure!!!
+
+        //Refactor to take into account the queue structure!!!
         //if(theTextManager.currentLine > theTextManager.endAtLine)
-		if(!theTextManager.getIsActive() || (theTextManager.currentLine > theTextManager.endAtLine))
+        if (!theTextManager.getIsActive() || (theTextManager.currentLine > theTextManager.endAtLine))
         {
-			textWasManuallyActivated = false;
-			//i.e., if the text isn't even activated any more, ofc it's not manually activated
+            textWasManuallyActivated = false;
+            //i.e., if the text isn't even activated any more, ofc it's not manually activated
         }
 
-		if (talkBubble != null && useSpeechBubble && (gameObject.name == theTextManager.getLastTriggered())) {
-			Debug.Log ("The text box is active, was last triggered by " + theTextManager.getLastTriggered() + ", so let's turn on the talk bubble");
-			talkBubble.SetActive (theTextManager.getIsActive() );
-		}
+        if (talkBubble != null && useSpeechBubble && (gameObject.name == theTextManager.getLastTriggered()))
+        {
+            Debug.Log("The text box is active, was last triggered by " + theTextManager.getLastTriggered() + ", so let's turn on the talk bubble");
+            talkBubble.SetActive(theTextManager.getIsActive());
+        }
+
+        if (destroyNextTimeTextboxCloses && !theTextManager.getIsActive())
+        {
+            talkBubble.SetActive(false);
+            Destroy(gameObject);
+        }
+
     }
 
     void OnTriggerEnter(Collider other)
@@ -79,23 +93,45 @@ public class ActivateTextAtLine : MonoBehaviour {
         //Debug.Log("ActivateTextAtLine's OnTriggerEnter triggered by: " + other.name);
         if (other.name == activatedByName || (tagTriggersText && other.CompareTag(activatedByTag)))
         {
-            if(requireButtonPress)
+            if (requireButtonPress)
             {
                 theTextManager.EnableCue();
                 waitForPress = true;
                 return;
             }
 
-			this.Activate();
+            this.Activate();
 
-            if(destroyWhenActivated)
+            if (destroyWhenFinished)
             {
-				//Having the stuff that activates the text bubble in the update loop causes problems. uh oh. dunno how to fix yet
+                destroyNextTimeTextboxCloses = true;
+            }
+
+            if (destroyWhenActivated)
+            {
+                //Having the stuff that activates the text bubble in the update loop causes problems. uh oh. dunno how to fix yet
+
+                /**
+                 * The below is a partial fix. The outstanding issue is that once that balloon activates, this script is effectively over.
+                 * Therefore, there's nothing available to make the balloon DISAPPEAR
+                 * 
+                 * What I need is someway to check if the text box is FINISHED.
+                 * 
+                 * I need to brainstorm some fixes: Maybe rather than "destroy when activated" I can do "destroy when finished"?
+                 * 
+
+                if (talkBubble != null && useSpeechBubble && (gameObject.name == theTextManager.getLastTriggered()))
+                {
+                    Debug.Log("The text box is active, was last triggered by " + theTextManager.getLastTriggered() + ", so let's turn on the talk bubble");
+                    talkBubble.SetActive(theTextManager.getIsActive());
+                }
+
+                 */
                 Destroy(gameObject);
             }
         }
     }
-    
+
     void OnTriggerExit(Collider other)
     {
         Debug.Log("ActivateTextAtLine's OnTriggerEnter triggered by: " + other.name);
@@ -108,8 +144,8 @@ public class ActivateTextAtLine : MonoBehaviour {
 
     private void Activate()
     {
-		theTextManager.setLastTriggered (gameObject.name);
-		theTextManager.setNPCName (NPCName);
+        theTextManager.setLastTriggered(gameObject.name);
+        theTextManager.setNPCName(NPCName);
         theTextManager.ReloadScript(theText);
         theTextManager.currentLine = startLine;
         theTextManager.endAtLine = endLine;

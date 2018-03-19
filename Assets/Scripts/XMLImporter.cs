61e022b086681714684119b06ccc75e832c61895
Keegan-Thinkpad\Keegan <keeganland@gmail.com>
2018-03-18 18:43:00 -0700

    Some nice basic starting points for the XML test I'm making
    Former-commit-id: 2dab9160ea6e371a1812d9509c86b3b9bb379f52
    Former-commit-id: 7bc22c8f78c403bd080637126674566f57f956bf

---
+++ Assets/Scripts/XMLImporter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+/**
+ * Temporary. Integrate with the text manager eventually.
+ * 
+ * see:
+ * http://amalgamatelabs.com/Blog/2/unity_and_xml
+ * https://www.youtube.com/watch?v=SrdrtsRYtPA&t=535s
+ */
+
+public class XMLImporter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void LoadSceneData()
+    {
+        TextAsset textAsset = (TextAsset)Resources.Load("test");
+        XmlDocument xmlDocument = new XmlDocument();
+        xmlDocument.LoadXml(textAsset.text);
+
+    }
+}

92882365d534739e71765c34194aecdb87967219
Keegan-NewDesktop <keeganland@gmail.com>
2018-03-27 14:21:57 -0700

    Title Screen can start the intro cutscene
    Former-commit-id: 275f4f981d8d5169762d7b3b75e805b7d48611bc
    Former-commit-id: 825a9ed28a933ebfa1b2459e284889024bc9f3b2

---
+++ Assets/Scripts/TitleScreenMainMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenMainMenu : MonoBehaviour {
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Introduction (1)");
+    }
+
+
+}

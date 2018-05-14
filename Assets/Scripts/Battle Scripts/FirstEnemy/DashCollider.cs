4f4962e5b470ac0e20475b6372922df681d6c693
Salade_de_Poney <alexandre.g007@gmail.com>
2018-05-14 15:57:54 -0700

    Alex : Healing VFX
    Former-commit-id: f32450eb111be1cd1f5ce3e691c3adcb8e79073f
    Former-commit-id: d44f18dbfa0973f61b4575aef28109eedc385fe3

---
+++ Assets/Scripts/Battle Scripts/FirstEnemy/DashCollider.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashCollider : MonoBehaviour {
+
+    private Rigidbody rb;
+    private GameObject spellCaster;
+    private bool isCollided;
+    private int AggroValue;
+
+    [SerializeField]
+    private float speed;
+    public GameObject target;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (target.activeSelf == false)
+        {
+            Destroy(gameObject, 1);
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (!isCollided)
+        {
+            Vector3 direction = target.transform.position - transform.position;
+            transform.position = Vector3.Lerp(this.transform.position, target.transform.position, speed * Time.fixedDeltaTime);
+       
+        }
+    }
+
+    public void SetTarget(GameObject passedTarget)
+    {
+        target = passedTarget;
+    }
+}

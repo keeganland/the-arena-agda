55db8c45610ecb6259cb6621b1fad17fdc46cfee
Ryan Donnelly <rdonnelly98@gmail.com>
2018-05-13 17:17:37 -0700

    brain-dead enymy
    Former-commit-id: 3097a2ada328981e427d8122e6e90fb2fe65f7d6
    Former-commit-id: 460623e6a7e1467e224e274173067eb04df30711

--- Assets/Scripts/Battle Scripts/FirstEnemy/FirstEnemyAttack.cs
+++ Assets/Scripts/Battle Scripts/FirstEnemy/FirstEnemyAttack.cs
@@ -6,6 +6,7 @@ using UnityEngine.AI;
 public class FirstEnemyAttack : MonoBehaviour {
 
     public Vector3 m_targetpos;
+	public Rigidbody enemy;
 
     [Header("Targeting and Attacks Data")]
     public Transform[] _Target;
@@ -15,105 +16,181 @@ public class FirstEnemyAttack : MonoBehaviour {
     public float _WarningtoAttackCD;
 
     private bool m_isAttacking;
-    private float m_timer;
+    private float m_timer; 
 
-    NavMeshAgent m_nav;
-    int i;
-    bool isCollided;
+    [Header("Linerenderers Variables")]
 
-    private GameObject[] _Targets;
-    private bool m_isChevreSpell;
-    private float m_soundFX;
+    private LineRenderer lineRenderer;
+    private float counter;
+    private float dist;
+    private bool m_linedrawing;
 
-    private AudioSource m_audioSource;
-    public AudioClip[] _AudioClip;
+    public float lineDrawSpeed = 6f;
 
-    public int Damage;
-    public int Aggro;
-    public float AttackSpeed;
-    public GameObject _Sprite;
-    private float AttackTimer;
+	NavMeshAgent m_nav;
+	int i;
+	bool isCollided;
+	private bool m_isDashAttack;
+
+	public GameObject _WarningDash;
 
 
-    //-----------------Alex Modifications-----------//
-    [SerializeField]
-    private GameObject[] spellPrefab;
+    // Use this for initialization
+    void Start ()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetWidth(2f, 2f);
+        lineRenderer.SetVertexCount(2);
+        lineRenderer.sortingOrder = 10;
 
+    }
+	
+	// Update is called once per frame
 	void Update ()
     {
-        if (!isCollided)
+		if (!isCollided)
+		{
+			if (i == 1 && _Target[i].GetComponent<HealthController>().currentHealth == 0 )
+			{
+				i = 0;
+			}
+			else if (i == 0 && _Target[i].GetComponent<HealthController>().currentHealth == 0)
+			{
+				i = 1;
+			}
+			if (_Target[i])
+			{
+				m_nav.SetDestination(_Target[i].transform.position);
+			}
+
+		}
+		
+		if (!m_isAttacking && m_timer >= _AttackCD)
         {
-            if (i == 1 && _Targets[i].GetComponent<HealthController>().currentHealth == 0)
-            {
-                i = 0;
-            }
-            else if (i == 0 && _Targets[i].GetComponent<HealthController>().currentHealth == 0)
-            {
-                i = 1;
-            }
-            if (_Targets[i])
-            {
-                m_nav.SetDestination(_Targets[i].transform.position);
-            }
+            Debug.Log("here");
+            m_isAttacking = true;
+            StartCoroutine("DashAttack");
         }
 
-        //Debug.Log("MeleeDamage: Damage Target" + m_target.name);
-        AttackTimer += Time.deltaTime; //including this although I'm not sure I have to. Should the animation Timer do this for me?
-        if (AttackTimer >= AttackSpeed && (this.GetComponentInChildren<RangeChecker>().InRange(_Targets[i])) && _Targets[i] != null)
+        if (m_linedrawing)
         {
-            if (!m_isChevreSpell)
-            {
-                StartCoroutine(DashAttack());
-            }
+            //get the unit vector in the desired direction, multiply by the desired length and add the starting point
+            //Vector3 pointAlongLine = x * Vector3.Normalize(pointB - pointA) + pointA;
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.SetPosition(1, m_targetpos);
         }
-
+        m_timer += Time.deltaTime;
 	}
+
+	private void CancelDashMovement()
+	{
+		isCollided = true;
+		m_nav.SetDestination(m_nav.transform.position);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (_Target[i])
+		{
+			if (other == _Target[i].GetComponent<Collider>())
+			{
+				//Debug.Log("Target in Range " + curTarget.name);
+				CancelDashMovement();
+				//Pass attack function here?
+			}
+			else return;
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (_Target[i])
+		{
+			//Debug.Log("TriggerExit" + other.name);
+			if (other == _Target[i].GetComponent<Collider>())
+			{
+				isCollided = false;
+				//will have player chase target once target leaves attack range trigger
+				//Debug.Log("Target out of range " + curTarget.name);
+			}
+		}
+	}
+
+	public int Damage;
+	public int Aggro;
+	public float AttackSpeed;
+	public GameObject _Sprite;
+	private float AttackTimer;
 
     private IEnumerator DashAttack()
     {
-        m_isChevreSpell = true;
+		m_isDashAttack = true;
 
-        Vector3 direction = _Targets[i].transform.position - transform.position;
-        float angle = Mathf.Atan2(direction.z, direction.x) * Mathf.Rad2Deg;
+		yield return new WaitForSeconds(1);
 
-        transform.LookAt(_Targets[i].transform);
+		Vector3 direction = _Target[i].transform.position - transform.position;
+		float angle = Mathf.Atan2(direction.z, direction.x) * Mathf.Rad2Deg;
 
-        int rotation = 0;
+		transform.LookAt(_Target[i].transform);
 
-        if (45 <= transform.eulerAngles.y && transform.eulerAngles.y < 135)
-        {
-            rotation = 3;
-        }
-        else if (0 <= transform.eulerAngles.y && transform.eulerAngles.y < 45)
-        {
-            rotation = 1;
-        }
-        else if (225 <= transform.eulerAngles.y && transform.eulerAngles.y < 315)
-        {
-            rotation = 4;
-        }
-        else
-        {
-            rotation = 2;
-        }
+		int rotation = 0;
 
-        if (rotation != 0)
-        {
-            _Sprite.GetComponent<SpriteScript2>().ForcePlayerRotation(rotation);
-        }
+		if (45 <= transform.eulerAngles.y && transform.eulerAngles.y < 135)
+		{
+			rotation = 3;
+		}
+		else if (0 <= transform.eulerAngles.y && transform.eulerAngles.y < 45)
+		{
+			rotation = 1;
+		}
+		else if (225 <= transform.eulerAngles.y && transform.eulerAngles.y < 315)
+		{
+			rotation = 4;
+		}
+		else
+		{
+			rotation = 2;
+		}
+
+		if (rotation != 0)
+		{
+			_Sprite.GetComponent<SpriteScript2>().ForcePlayerRotation(rotation);
+		}
+
+		_WarningDash.SetActive(true);
+
+		DamageData dmgData = new DamageData();
+		dmgData.damage = Damage;
+
+		MessageHandler msgHandler = _Target[i].GetComponent<MessageHandler>();
+
+		if (msgHandler)
+		{
+			msgHandler.GiveMessage(MessageTypes.DAMAGED, this.gameObject, dmgData);
+		}
+		AttackTimer = 0.0f;
+
+		m_isDashAttack = false;
 
 
-        //start Attack
-        GameObject go = Instantiate(spellPrefab[0], _Targets[i].transform.position, Quaternion.Euler(0, -angle, 0)); //warning
+        Debug.Log("Coroutine Started");
 
-        yield return new WaitForSeconds(_WarningtoAttackCD);
+        //We select the position for the spell
+        _BoyOrGirl = Random.Range(0, 2);
+        m_targetpos = _Target[_BoyOrGirl].position;
 
-        
+        Debug.Log(m_targetpos);
+        lineRenderer.enabled = true;
 
-        AttackTimer = 0.0f;   
-        m_isChevreSpell = false;
+        //Look toward target and draw "warning" line
+        transform.LookAt(_Target[_BoyOrGirl]);
+        m_linedrawing = true;
+
+        yield return new WaitForSeconds(_WarningtoAttackCD); 
+
+        //Start the Attack
+		enemy.AddForce(transform.forward * 750);
     }
-
 }
-
 

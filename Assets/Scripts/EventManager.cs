0868c943483aaf4fec32060d51550425cbdc928e
Keegan-NewDesktop <keeganland@gmail.com>
2018-05-19 21:53:42 -0700

    Playing around with EventManager from unity website. Script annotated with references
    Former-commit-id: 0c98f98ca160db7a0ccaede2b0aa1f2837db76c1
    Former-commit-id: 1e379da2eea075c1a69e71fd800db96a0950926f

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,88 @@
+﻿/*
+ * Stealing from : https://unity3d.com/learn/tutorials/topics/scripting/events-creating-simple-messaging-system
+ * 
+ */
+
+using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EventManager : MonoBehaviour
+{
+    //originally was: 
+    //private Dictionary  eventDictionary;
+    //Changed later constructor call accordingly
+    private Dictionary<string, UnityEvent> eventDictionary;
+
+    private static EventManager eventManager;
+
+    //Note: original had lowercase instance throughout. Auto-suggest said capitalize as I all throughout
+    /* Keegan NTS: I'm so not used to C# singleton patterns! But that's what's going on here
+     * Refer to: https://msdn.microsoft.com/en-us/library/ff650316.aspx
+     * 
+     */
+    public static EventManager Instance
+    {
+        get
+        {
+            if (!eventManager)
+            {
+                eventManager = FindObjectOfType(typeof(EventManager)) as EventManager;
+
+                if (!eventManager)
+                {
+                    Debug.LogError("There needs to be one active EventManger script on a GameObject in your scene.");
+                }
+                else
+                {
+                    eventManager.Init();
+                }
+            }
+
+            return eventManager;
+        }
+    }
+
+    void Init()
+    {
+        if (eventDictionary == null)
+        {
+            eventDictionary = new Dictionary<string, UnityEvent>();
+        }
+    }
+
+    public static void StartListening(string eventName, UnityAction listener)
+    {
+        UnityEvent thisEvent = null;
+        if (Instance.eventDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.AddListener(listener);
+        }
+        else
+        {
+            thisEvent = new UnityEvent();
+            thisEvent.AddListener(listener);
+            Instance.eventDictionary.Add(eventName, thisEvent);
+        }
+    }
+
+    public static void StopListening(string eventName, UnityAction listener)
+    {
+        if (eventManager == null) return;
+        UnityEvent thisEvent = null;
+        if (Instance.eventDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.RemoveListener(listener);
+        }
+    }
+
+    public static void TriggerEvent(string eventName)
+    {
+        UnityEvent thisEvent = null;
+        if (Instance.eventDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.Invoke();
+        }
+    }
+}

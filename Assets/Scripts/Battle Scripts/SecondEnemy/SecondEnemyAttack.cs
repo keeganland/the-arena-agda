f85c4f91c9b44051aa9b49d1dde3f97017beba53
Salade_de_Poney <alexandre.g007@gmail.com>
2018-06-03 19:12:48 -0700

    Alex : 2nd enemy
    Former-commit-id: ed8b0692a4635898e4440614993836ee67c61d17
    Former-commit-id: 16712769f747ef72432b1ee27317c6874a7bb203

--- Assets/Scripts/Battle Scripts/SecondEnemy/SecondEnemyAttack.cs
+++ Assets/Scripts/Battle Scripts/SecondEnemy/SecondEnemyAttack.cs
@@ -5,34 +5,70 @@ using UnityEngine.UI;
 
 public class SecondEnemyAttack : MonoBehaviour {
 
-    public PublicVariableHolderArena publicVariableHolderArena;
-    public float spawnCD;
-    public float teleportCD;
     public Slider _CastSpellSlider;
     public GameObject _CastSpellGameobject;
     public Text _SpellCasttimer;
 
+    public PublicVariableHolderArena publicVariableHolderArena;
+
+    public Color RedSheepcolor;
+
     private GameObject SheepPrefab;
     private Transform[] TeleportPosition;
     private Transform[] SpawnPosition;
-    private float time;
+    private float spawnTime;
+    private float teleportTime;
+    private int currentTeleportPosition;
+
+    [Header("Totem Characteristics")]
+    public float spawnCD = 30;
+    public float teleportCD = 180;
+    [Header("Sheep Characteristics")]
+    public int Sheephealth = 10;
+    public int RedSheephealth = 55;
 
 	// Use this for initialization
 	void Start () { 
 
         SheepPrefab = publicVariableHolderArena._SheepPrefab;
         TeleportPosition = publicVariableHolderArena._TotemTeleportPos;
+        SpawnPosition = publicVariableHolderArena._Spawnposition;
+
+        spawnTime = 20;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
-        if(time>= spawnCD)
+        if(spawnTime >= spawnCD)
         {
-            GameObject sheep1 = Instantiate(SheepPrefab);
-            GameObject sheep2 = Instantiate(SheepPrefab);
-            GameObject sheep3 = Instantiate(SheepPrefab);
+            GameObject sheep1 = Instantiate(SheepPrefab, SpawnPosition[0]);
+            GameObject sheep2 = Instantiate(SheepPrefab, SpawnPosition[1]);
+            GameObject sheep3 = Instantiate(SheepPrefab, SpawnPosition[2]);
+
+            GameObject Redsheep = sheep3.GetComponent<HealthController>().Sprite;
+
+            Redsheep.GetComponent<SpriteRenderer>().color = RedSheepcolor;
+
+            sheep1.GetComponent<HealthController>().currentHealth = Sheephealth;
+            sheep2.GetComponent<HealthController>().currentHealth = Sheephealth;
+            sheep3.GetComponent<HealthController>().currentHealth = RedSheephealth;
+
+            spawnTime = 0;
         }
+        if(teleportTime >= teleportCD)
+        {
+            int i = Random.Range(0, TeleportPosition.Length + 1);
+            if (currentTeleportPosition != i)
+            {
+                transform.position = TeleportPosition[i].position;
+                currentTeleportPosition = i;
+                teleportTime = 0;
+            }
+        }
+
+        spawnTime += Time.deltaTime;
+        teleportTime += Time.deltaTime;
 	}
 }
 

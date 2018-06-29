62d023295eb40f52a02db88b2acf726ec96f7ce6
Salade_de_Poney <alexandre.g007@gmail.com>
2018-06-29 16:54:24 -0700

    Alex
    Former-commit-id: 5f03ac24f5a7bf4d770584eb1fc6267052d18e36
    Former-commit-id: cf5294e69824c61f6ddc13c2bbe98110338dc61e

---
+++ Assets/Scripts/System/ChangeRoom.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeRoom : MonoBehaviour {
+
+    Collider m_collider;
+    Animator m_anim;
+    bool m_isFading;
+
+	void Start () {
+
+        m_collider = GetComponent<Collider>();
+        m_anim = GetComponent<Animator>();
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            FadeRoomIn();
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+           FadeRoomOut();
+        }
+    }
+
+    private void FadeRoomIn()
+    {
+        m_anim.SetBool("FadeOut", false);
+        m_anim.SetBool("FadeIn", true);
+    }
+
+    private void FadeRoomOut()
+    {
+        m_anim.SetBool("FadeIn", false);
+        m_anim.SetBool("FadeOut", true);
+    }
+}

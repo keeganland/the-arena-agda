13bb5b8af7a575e898f1db29d2b4cb5fc70b6864
Keegan-NewDesktop <keeganland@gmail.com>
2018-07-01 15:01:38 -0700

    FightTracker script on Enemy game object contains references to all the individual enemy gameobjects
    Former-commit-id: 66b2707e1ea8942460a2ed35876762b94339f9f2
    Former-commit-id: 43cb0e9002876b61a2eaff5634a37ee1f4f6d693

---
+++ Assets/FightTracker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FightTracker : MonoBehaviour {
+
+    public List<GameObject> enemies; //Keegan 2018/7/1: switch to private once testing is done
+    
+
+    protected void OnEnable()
+    {
+        enemies = new List<GameObject>();
+
+    }
+
+    // Use this for initialization
+    void Start() {
+        /* Refernces used:
+         * https://answers.unity.com/questions/594210/get-all-children-gameobjects.html
+         * https://answers.unity.com/questions/205391/how-to-get-list-of-child-game-objects.html
+         */
+
+        foreach (Transform child in transform)
+        {
+            enemies.Add(child.gameObject);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

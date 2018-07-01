6d4d560852de6b6fbc98bdd7d7d91006e5f705ac
Keegan-NewDesktop <keeganland@gmail.com>
2018-07-01 16:36:48 -0700

    fight load test menu accessible by F1 button
    Former-commit-id: 8db4b561a827e8fe7ba4938143e18317b9c02ffe
    Former-commit-id: e3ba4ef77e3a1c8d99646c7c64cccd9c2bc1868f

---
+++ Assets/FightLoaderTester.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FightLoaderTester : MonoBehaviour {
+
+    public static bool gameIsPaused = false;
+    public GameObject fightMenuUI;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    private void Resume()
+    {
+        fightMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    private void Pause()
+    {
+        fightMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+}

ad37412fa040ab3523c15498292fcb36858e465b
Keegan-Thinkpad\Keegan <keeganland@gmail.com>
2018-07-02 18:41:58 -0700

    Moved prefabs to resources folder. Sourcetree thinks this is big!
    Former-commit-id: 8e86018292eea948f9fcc251cb48191afe2982bf
    Former-commit-id: 8874e9f91638b384b7c115aab156665642512c88

---
+++ Assets/EnemyLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLoader : MonoBehaviour {
+
+    public PublicVariableHolderArena publicVariableHolderArena;
+
+    private List<GameObject> EnemyPrefabs;
+
+
+    private void OnEnable()
+    {
+        EnemyPrefabs = new List<GameObject>();
+        EnemyPrefabs.Add(publicVariableHolderArena._SheepPrefab);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+}

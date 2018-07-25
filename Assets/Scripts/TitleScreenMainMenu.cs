91656b6a63921337e29b0984be98f96e0bb2327e
Alexandre Guichet <alexandre.g007@gmail.com>
2018-07-25 23:47:58 +0200

    Alex : Work on TitleScreen
    Former-commit-id: be15625fc5c98efa0c1f3f47af6d267d21c34348
    Former-commit-id: 8804eb6ae274c717e2e3799020c1ac0de41a6bfb

--- Assets/Scripts/TitleScreenMainMenu.cs
+++ Assets/Scripts/TitleScreenMainMenu.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class TitleScreenMainMenu : MonoBehaviour {
+    
+    private void Start()
+    {
+        GameObject.Find("PlayerUI").SetActive(false);
+    }
 
     public void PlayGame()
     {
@@ -39,10 +44,29 @@ public class TitleScreenMainMenu : MonoBehaviour {
         SceneManager.LoadSceneAsync("Dialog - Prototype", LoadSceneMode.Additive);
         EventManager.TriggerEvent("setup");
     }
+    public void PlayHowToPlay()
+    {
+        SceneManager.UnloadSceneAsync("TitleScreen");
+        SceneManager.LoadSceneAsync("HowToPlay", LoadSceneMode.Additive);
+        EventManager.TriggerEvent("setup");
+    }
 
     public void QuitGame ()
     {
         Debug.Log("Game Quit!");
         Application.Quit();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.collider.name);
+            }
+        }   
     }
 }

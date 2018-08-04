966d117685d6b4d3fa0e8c7cffd038b25c7c1dca
Keegan-NewDesktop <keeganland@gmail.com>
2018-08-03 20:26:28 -0700

    New WIP script. Singleton class that will have references to all the things that need to be "reset" when a new fight loads, a new scene loads, etc. so that we can "clean the slate"/"clear the board". Will also contain specific modifiers so we can clean said slate for specific fights. That's why it contains the variables that tell the Arena what enemies to load when the arena loads
    Former-commit-id: b65637ffd86874530eb8e0715f392cda632ac53c
    Former-commit-id: 7dc297d23d151af6aeb25f324434694c2491dd36

---
+++ Assets/SetUp.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetUp : MonoBehaviour {
+
+    private static int fightToLoad;
+    private static SetUp setUp;
+
+    public static SetUp Instance
+    {
+        get
+        {
+            if (!setUp)
+            {
+                setUp = FindObjectOfType(typeof(SetUp)) as SetUp;
+
+                if (!setUp)
+                {
+                    Debug.LogError("There needs to be one active SetUp script on a GameObject in your scene.");
+                }
+                else
+                {
+                    setUp.Init();
+                }
+            }
+            return setUp;
+        }
+    }
+
+    void Init()
+    {
+        fightToLoad = 0;
+    }
+
+    // Use this for initialization
+    void Start () {
+        EventManager.StartListening("setup", SetUpCharacterHealth);
+	}
+
+    private void OnDisable()
+    {
+        EventManager.StopListening("setup", SetUpCharacterHealth);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void SetUpCharacterHealth()
+    {
+        /*TODO reset the players' health here*/
+    }
+
+    public int GetFightToLoad()
+    {
+        return fightToLoad;
+    }
+
+    public void SetFightToLoad(int n)
+    {
+        fightToLoad = n;
+    }
+}

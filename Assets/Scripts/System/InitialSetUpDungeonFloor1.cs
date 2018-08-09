fa157b0e406e1722d5edecd937e8f5f04a09f5ba
Alexandre Guichet <alexandre.g007@gmail.com>
2018-08-09 19:40:33 +0200

    Alex : Small fixes
    Former-commit-id: 18a03f96d4589478c96b358995df38111e8ea57e
    Former-commit-id: 12e9f55142a18820ea945b577d10db0372bb15f3

---
+++ Assets/Scripts/System/InitialSetUpDungeonFloor1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitialSetUpDungeonFloor1 : InitialSceneSetup {
+
+    private GameObject SpawnPosBoy;
+    private GameObject SpawnPosGirl;
+
+    new void Start()
+    {
+        base.Start();
+
+        SpawnPosBoy = publicArenaEntrance.SpawnPosBoy;
+        SpawnPosGirl = publicArenaEntrance.SpawnPosGirl;
+
+        Boy.transform.position = new Vector3(SpawnPosBoy.transform.position.x, transform.position.y, SpawnPosBoy.transform.position.z);
+        Girl.transform.position = new Vector3(SpawnPosGirl.transform.position.x, transform.position.y, SpawnPosGirl.transform.position.z);
+
+        BoyNav.enabled = true;
+        GirlNav.enabled = true;
+
+        Boy.GetComponent<BetterPlayer_Movement>().isCombat = false;
+        Girl.GetComponent<BetterPlayer_Movement>().isCombat = false;
+
+        MainCamera = publicArenaEntrance.MainCamera;
+
+        MainCamera.GetComponent<BetterCameraFollow>()._FieldOfViewMin = MainCameraFieldOfViewMin;
+        MainCamera.GetComponent<BetterCameraFollow>()._FieldOfViewMax = MainCameraFieldOfViewMax;
+
+        publicArenaEntrance.publicVariableHolderNeverUnload.PlayerUI.SetActive(false);
+
+        publicArenaEntrance.publicVariableHolderNeverUnload.fader.StartCoroutine("FadeIn");   
+	}
+}

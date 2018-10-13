c5e1e31bd4efef25ca160b95f0dd685ae6d5b554
Alexandre Guichet <alexandre.g007@gmail.com>
2018-10-13 16:30:50 -0700

    Alex: click & drag to select characters
    Former-commit-id: f8350a764ad8e68d5cf1f3270f22ff5e4fbc473f

---
+++ Assets/Scripts/System/DragHandler.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerClickHandler {
+
+    [SerializeField]
+    Image selectionBoxImage;
+    Rect selectrionRect;
+
+    private Vector2 oldMousePosition;
+    private Vector2 newMousePosition;
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        selectionBoxImage.gameObject.SetActive(false);
+        foreach (SelectableObjects selectable in SelectableObjects.allSelectableObjects)
+        {
+            if (selectrionRect.Contains(Camera.main.WorldToScreenPoint(selectable.transform.position)))
+            {
+                selectable.OnSelect(eventData);
+            }
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (eventData.position.x < oldMousePosition.x)
+        {
+            selectrionRect.xMin = eventData.position.x;
+            selectrionRect.xMax = oldMousePosition.x;
+        }
+        else
+        {
+            selectrionRect.xMin = oldMousePosition.x;
+            selectrionRect.xMax = eventData.position.x;
+        }
+
+        if (eventData.position.y < oldMousePosition.y)
+        {
+            selectrionRect.yMin = eventData.position.y;
+            selectrionRect.yMax = oldMousePosition.y;
+        }
+        else
+        {
+            selectrionRect.yMin = oldMousePosition.y;
+            selectrionRect.yMax = eventData.position.y;
+        }
+
+        selectionBoxImage.rectTransform.offsetMin = selectrionRect.min;
+        selectionBoxImage.rectTransform.offsetMax = selectrionRect.max;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (!Input.GetKey(KeyCode.LeftControl) && !Input.GetKey(KeyCode.RightControl))
+        {
+            SelectableObjects.DeselectAll(new BaseEventData(EventSystem.current));
+        }
+        selectionBoxImage.gameObject.SetActive(true);
+        oldMousePosition = eventData.position;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, results);
+
+        float myDistance = 0;
+
+        foreach (RaycastResult result in results)
+        {
+            if (result.gameObject == gameObject)
+            {
+                myDistance = result.distance;
+                break;
+            }
+        }
+
+        GameObject nextObject = null;
+        float maxDistance = Mathf.Infinity;
+
+        foreach (RaycastResult result in results)
+        {
+            if (result.distance > myDistance && result.distance < maxDistance)
+            {
+                nextObject = result.gameObject;
+                maxDistance = result.distance;
+            }
+        }
+
+        if (nextObject)
+        {
+            ExecuteEvents.Execute<IPointerClickHandler>(nextObject, eventData, (x, y) => { x.OnPointerClick((PointerEventData)y); });
+        }
+    }
+}
---
+++ Assets/Scripts/System/SelectableObjects.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+// Tutorial https://www.youtube.com/watch?v=ln03y7sJ_OE
+
+public class SelectableObjects : MonoBehaviour, ISelectHandler, IPointerClickHandler, IDeselectHandler {
+
+    public static HashSet<SelectableObjects> allSelectableObjects = new HashSet<SelectableObjects>();
+    public static HashSet<SelectableObjects> currentlySelected = new HashSet<SelectableObjects>();
+
+    Renderer myRenderer;
+
+
+    void Awake()
+    {
+        allSelectableObjects.Add(this);
+        myRenderer = GetComponent<Renderer>();
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (!Input.GetKey(KeyCode.LeftControl) && !Input.GetKey(KeyCode.RightControl))
+        {
+            DeselectAll(eventData);
+        }
+        OnSelect(eventData);
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+       currentlySelected.Add(this);
+    }
+
+    public static void DeselectAll(BaseEventData eventData)
+    {
+        foreach(SelectableObjects selectable in currentlySelected)
+        {
+            selectable.OnDeselect(eventData);
+        }
+        currentlySelected.Clear();
+    }
+}
+
+
